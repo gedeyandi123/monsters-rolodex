a6b52190ce7419f91a3eb469c383811deda00585
gedeyandi123 <g3d3.development@gmail.com>
2019-09-14 16:39:08 +0800

    Updates

---
+++ precache-manifest.a031cd15c9eaec52cb17f5aaa77ec2c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d62051884eb64af6304b30d51e79045",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6758f6a8fe61f44b42a4",
+    "url": "/monsters-rolodex/static/css/main.16045e5f.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "6758f6a8fe61f44b42a4",
+    "url": "/monsters-rolodex/static/js/main.d11ecb78.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
